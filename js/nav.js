490586770c29909dfcbaa068b022bdb332611aae
Jake Close <jsclose@umich.edu>
2017-02-12 16:59:26 -0500

    Working on getting pages to hide/show with nav

--- js/nav.js
+++ js/nav.js
@@ -1,13 +1,27 @@
-/**
- * Created by jakeclose on 2/12/17.
- */
+
+
+
+
+
 
 
 
 $("#next_module").click(function () {
-    $next_module =  $("li.active").next();
+    $next_module_tab =  $("li.active").next();
     $("li.active").removeClass("active");
-    $next_module.attr("class", "active")
+    $next_module_tab.attr("class", "active");
+
+
+
+
+    $next_module_page =  $("li.module").next();
+    $("li.module").css("visibility","hidden");
+    $("li.module").removeClass("module");
+    $next_module_page.attr("class", "module");
+    $next_module_page.css("visibility","visible");
+
+
+
 
 });
 
@@ -21,8 +35,15 @@ $("#next_module").click(function () {
 $("#previous_module").click(function () {
     $prev_module =  $("li.active").prev();
     $("li.active").removeClass("active");
-    $prev_module.attr("class", "active")
+    $prev_module.attr("class", "active");
 
+
+
+    $prev_module_page =  $("li.module").prev();
+    $("li.module").css("visibility","hidden");
+    $("li.module").removeClass("module");
+    $prev_module_page.attr("class", "module");
+    $prev_module.css("visibility","visible")
 
 
 });
