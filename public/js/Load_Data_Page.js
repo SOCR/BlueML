f1dcd788d7f338acea1381ff3339670ea71bf3a4
Matthew Chen <mattchen@umich.edu>
2018-04-15 02:17:21 -0400

    Implemented Bitstream to JSON conversion

--- public/js/Load_Data_Page.js
+++ public/js/Load_Data_Page.js
@@ -1,5 +1,6 @@
 /**
  * Created by jakeclose on 2/14/17.
+ * Edited by thematthewchen in March 2018
  */
 $(function () {
     $("#add_another_upload_button").click(function () {
@@ -15,10 +16,27 @@ $(function () {
         var fileName = e.target.files[0].name;
         var reader = new FileReader();
         reader.onload = function(e) {
-          var rawData = reader.result; //rawData holds binary information
+            //Reads in data from inputted file
+            rawData = reader.result; //rawData holds information, global
+            var parse = new Parser(); //create new instance of Parser class in parser.js
+            /* 
+            *
+            * TODO: JSON is based off of aa3mia.dat from https://www.physionet.org/physiobank/database/aami-ec13/
+            *       which has a format of 16 bits signed integers according to the header file
+            *       
+            *       Parsing and JSON format need to into account header files information
+            *       Temporarily set to reading 10 chunks of sshorts for each.
+            */
+            var result = {
+                'fips': parse.scan('sshort', rawData, 10), 
+                'Mal': parse.scan('sshort', rawData, 10),
+                'Femal': parse.scan('sshort', rawData, 10),
+                'Income': parse.scan('sshort', rawData, 10)
+            };
+            console.log(result)
         }
+        reader.readAsArrayBuffer(e.target.files[0]);
 
-        reader.readAsBinaryString(e.target.files[0]);
 
       //  alert('The file "' + fileName +  '" has been selected.');
         $(".file_upload tr *:nth-child(3)").show();
@@ -27,15 +45,7 @@ $(function () {
 
         remove_or_show_nav_buttons()
         $('#add_another_upload_button').attr("disabled", false);
-
-
-
     });
-
-
-
-
-
 });
 
 
