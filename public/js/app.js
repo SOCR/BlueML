e0b266768a9ea56c2531f1982774fad523e8aec2
Jiajun Peng <pjiajun@umich.edu>
2019-04-22 23:16:34 -0400

    no need for service

--- public/js/app.js
+++ public/js/app.js
@@ -1,2 +1,2 @@
 console.log("blueml app.js");
-angular.module('BlueML', ['ngRoute', 'appRoutes', 'ResultsCtrl', 'ResultsService']);
+angular.module('BlueML', ['ngRoute', 'appRoutes', 'ResultsCtrl']);
